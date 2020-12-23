10ffe7bda5168f528e61ef5d5dccbae538348319
AugustoCalado <augusto.calado11@gmail.com>
2020-12-22 21:45:37 -0300

    Added Lesson 6

---
+++ Spring-Boot-Basics/Lesson-6/Selenium-WebDriver-Project/src/main/java/aug/bueno/DemoApplication.java
@@ -0,0 +1,15 @@
+package aug.bueno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+
+
+	}
+
+}
